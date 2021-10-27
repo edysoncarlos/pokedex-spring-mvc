fd582785804041a647765f161a5f76361c357610
Edson Carlos Mahumane <ecmamalakh77@users.noreply.github.com>
2021-10-27 02:39:50 +0200

    cmt

--- src/main/java/net/pokedex/spring/config/MvcConfiguration.java
+++ src/main/java/net/pokedex/spring/config/MvcConfiguration.java
@@ -21,7 +21,7 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
-	
+	//
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
